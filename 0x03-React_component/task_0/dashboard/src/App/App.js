ab1839812dbcc9d242247803e42e37f44e533dca
Yoitsjae <ncubejason896@gmail.com>
2024-04-27 13:32:39 +0200

    0x03-React_component

---
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -0,0 +1,19 @@
+// task_0/dashboard/src/App/App.js
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* Your JSX code here */}
+      </div>
+    );
+  }
+}
+
+export default App;
